0e081e476f35c928741bd97ef388e04954e8f29b
B1ackGX <xueqianlin.0821@gmail.com>
2021-09-16 00:19:22 -0700

    Update queue.js

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -9,12 +9,13 @@ module.exports = {
         if (!queue) return message.channel.send(new Discord.MessageEmbed()
         .setTitle(`**Queue for ${message.guild}**`)
         .setDescription(`__Now Playing:__\nNothing Playing!`)
+        .setColor('RED')
         );
        
         let string = "";
 
         if(queue.songs[0]) string += `__Now Playing:__\n [${queue.songs[0].name}](${queue.songs[0].url})\n \`${queue.songs[0].formattedDuration} Requested by: ${queue.songs[0].user.tag}\``
-        if(queue.songs[1]) string += `\n__Up Next:__\n ${queue.songs.map((song, id) => `**${id}**. [${song.name}](${song.url})\n \`${song.formattedDuration} Requested by: ${song.user.tag}\``).slice(1, 5).join("\n")}`
+        if(queue.songs[1]) string += `\n__Up Next:__\n ${queue.songs.map((song, id) => `\`${id}.\` [${song.name}](${song.url})\n \`${song.formattedDuration} Requested by: ${song.user.tag}\``).slice(1, 10).join("\n")}`
 
         const embed = new Discord.MessageEmbed()
             .setTitle(`**Queue for ${message.guild}**`)
