cccca10db61e1b96686d6b85a342798d418d6d19
B1ackGX <65946525+B1ackGX@users.noreply.github.com>
2021-09-16 10:14:18 -0700

    Update loopqueue.js

--- commands/music/loopqueue.js
+++ commands/music/loopqueue.js
@@ -7,14 +7,14 @@ module.exports = {
     run: async (client, message, args) => {
         const queue = client.distube.getQueue(message)
         if (!queue) return message.channel.send(`**There is nothing playing!**`)
-        if(client.distube.setRepeatMode(message, 1)){
+        if(queue.repeatMode = 1){
             return
         }
-        if(client.distube.setRepeatMode(message, 0)){
+        if(queue.repeatMode 0){
             client.distube.setRepeatMode(message, 2)
             message.channel.send('🔄 **Loop Enabled**')
         }
-        if(client.distube.setRepeatMode(message, 2)){
+        else{
             client.distube.setRepeatMode(message, 0)
             message.channel.send('🔄 **Disabled**')
         }
