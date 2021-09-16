a6316125ba5f382db5c5d922b795acd970289a81
B1ackGX <xueqianlin.0821@gmail.com>
2021-09-15 23:47:12 -0700

    Update queue.js

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -8,21 +8,17 @@ module.exports = {
         const queue = client.distube.getQueue(message)
         if (!queue) return message.channel.send(new Discord.MessageEmbed()
         .setTitle(`**Queue for ${message.guild}**`)
-        .setDescription('Nothing Playing!')
+        .addField(`__Now Playing:__\nNothing Playing!`)
         );
-        const NP = queue.songs[0]
-        const CQ =  `__Now Playing:__\n**[${queue.songs[0].name}](${queue.songs[0].url})\n\`${queue.songs[0].formattedDuration} Requested by: ${queue.songs[0].user.tag}\``
-        for(let i = 1; i < queue.songs.length; i++){
-            description += `**${i}.** [${queue.songs[i].name}](${queue.songs[i].url}) | \`${queue.songs[i].formattedDuration}Requested by: ${queue.songs[i].user.tag}\`\n`
-            if (queue) return message.channel.send(new Discord.MessageEmbed()
-                .setTitle(`**Queue for ${message.guild}**`)
-                .addField(
-                { name: CQ},
-                { name: '\u200B', value: '\u200B' },
-                { name: description}
-            )
-        );
-        }
-        
+       
+        let string = "";
+
+        if(queue.songs[0]) string += `__Now Playing:__\n [${queue.songs[0].name}](${queue.songs[0].url})\n \`${queue.songs[0].formattedDuration} Requested by: ${queue.songs[0].user.tag}\``
+        if(queue.songs[1]) string += `__Up Next:__\n [${queue.songs.map((song, id) => `**${id + 1}**. [${song.name}](${song.url})\n \`${song.formattedDuration} Requested by: ${song.user.tag}\``).slice(1, 10).join("\n")}]`
+
+        const embed = new Discord.MessageEmbed()
+            .setTitle(`**Queue for ${message.guild}**`)
+            .addField(string);
+        return message.channel.send(embed)
     }
 }
