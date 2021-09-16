7d2dfc41afe1b130a87f5b5b6d7ac33a687a44f9
B1ackGX <xueqianlin.0821@gmail.com>
2021-09-16 00:14:24 -0700

    Update queue.js

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -14,7 +14,7 @@ module.exports = {
         let string = "";
 
         if(queue.songs[0]) string += `__Now Playing:__\n [${queue.songs[0].name}](${queue.songs[0].url})\n \`${queue.songs[0].formattedDuration} Requested by: ${queue.songs[0].user.tag}\``
-        if(queue.songs[1]) string += `__Up Next:__\n ${queue.songs.map((song, id) => `**${id + 1}**. [${song.name}](${song.url})\n \`${song.formattedDuration} Requested by: ${song.user.tag}\``).slice(1, 5).join("\n")}`
+        if(queue.songs[1]) string += `\n__Up Next:__\n ${queue.songs.map((song, id) => `**${id}**. [${song.name}](${song.url})\n \`${song.formattedDuration} Requested by: ${song.user.tag}\``).slice(1, 5).join("\n")}`
 
         const embed = new Discord.MessageEmbed()
             .setTitle(`**Queue for ${message.guild}**`)
