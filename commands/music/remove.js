2ae95837da7ecfa83cb61b05e516575c5333b2d7
B1ackGX <xueqianlin.0821@gmail.com>
2021-09-16 21:51:50 -0700

    Create remove.js

---
+++ commands/music/remove.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: "remove",
+    description: "Loop current queue",
+    run: async (client, message, args) => {
+        var queueN = `${args}`
+        if(!message.member.voice.channel){
+            return message.channel.send('❌ **Please join the voice channel!**');
+        }
+        if(client.distube.isPlaying(message) && message.member.voice.channel != message.guild.me.voice.channel){
+            return message.channel.send('❌ **You are not in the same voice channel as I am!**')
+        }
+        const queue = client.distube.getQueue(message)
+        if (!queue) {
+            return message.channel.send(`**There is nothing playing!**`)
+        }
+        if(isNaN(queueN) || !queueN){
+            return message.channel.send(new Discord.MessageEmbed()
+                .setDescription('❌ **Invalid Usage**')
+                .addField("\u200B", ".remove [index]")
+                .addField("\u200B", "Example: \`.remove 1\`")
+            )
+        }
+        if(queueN > queue.songs.length) {
+            return message.channel.send(new Discord.MessageEmbed()
+                .setDescription('❌ **Invalid Usage**')
+                .addField("\u200B", ".remove [index]")
+                .addField("\u200B", "Example: \`.remove 1\`")
+            )
+        }else {
+            queue.delete(queueN)
+        }
+    }
+}
