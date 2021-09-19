441ce7252c68d25f22049b75885f0916f5513d50
B1ackGX <xueqianlin.0821@gmail.com>
2021-09-18 20:12:03 -0700

    Update queue.js

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -5,8 +5,18 @@ module.exports = {
     aliases: ['q'],
     description: "Check the current queue!",
     run: async (client, message, args) => {
-        let l = ""
-        let lq = ""
+        
+        const queue = client.distube.getQueue(message)
+        if (!queue) return message.channel.send(new Discord.MessageEmbed()
+        .setTitle(`**Queue for ${message.guild}**`)
+        .setDescription(`__Now Playing:__\nNothing Playing!`)
+        .setColor('RANDOM')
+        );
+       
+        let string = "";
+
+        let l = "";
+        let lq = "";
 
         if(queue.repeatMode = 1){
             l = "✔️"
@@ -18,14 +28,6 @@ module.exports = {
         } else{
             lq = "❌"
         }
-        const queue = client.distube.getQueue(message)
-        if (!queue) return message.channel.send(new Discord.MessageEmbed()
-        .setTitle(`**Queue for ${message.guild}**`)
-        .setDescription(`__Now Playing:__\nNothing Playing!`)
-        .setColor('RANDOM')
-        );
-       
-        let string = "";
 
         if(queue.songs[0]) string += `__Now Playing:__\n [${queue.songs[0].name}](${queue.songs[0].url})\n \`${queue.songs[0].formattedDuration} Requested by: ${queue.songs[0].user.tag}\``
         if(queue.songs[1]) string += `\n__Up Next:__\n ${queue.songs.map((song, id) => `\`${id}.\` [${song.name}](${song.url})\n \`${song.formattedDuration} Requested by: ${song.user.tag}\``).slice(1, 10).join("\n")}`
