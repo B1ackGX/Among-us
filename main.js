aec7d3c9ae7b78d588666c0cdbaec03588df5196
B1ackGX <xueqianlin.0821@gmail.com>
2021-09-25 16:23:17 -0700

    Update main.js

--- main.js
+++ main.js
@@ -20,12 +20,43 @@ client.aliases = new Collection();
 
 client.on("ready", () => {
     console.log(`AMONG US!`);
+
     client.user.setActivity(`AMONG US!`) 
+})
+
+client.on('guildMemberAdd', member => {
+    member.send('**' + member.user.username + '** Welcome to the server!');
+});
+
+client.on("message", async message => {
+   
+
+    if (message.author.bot) return;
+    if (!message.guild) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    // If message.member is uncached, cache it.
+    if (!message.member) message.member = await message.guild.fetchMember(message);
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    
+    if (cmd.length === 0) return;
+    
+    // Get the command
+    let command = client.commands.get(cmd);
+    // If none is found, try to find it by alias
+    if (!command) command = client.commands.get(client.aliases.get(cmd));
+
+    // If a command is finally found, run the command
+    if (command) 
+        command.run(client, message, args);
+});
+    
     client.distube
-    .on("playSong", (message, queue, song) => {if (queue.songs[0].name == song.name){
-        return message.channel.send(`**Playing**:notes: \`${song.name}\` - Now! `)
-    } 
-    })
+    .once("playSong", (message, queue, song) => message.channel.send(
+        `**Playing**:notes: \`${song.name}\` - Now! `
+    ))
     .on("addSong", (message, queue, song) => message.channel.send(new Discord.MessageEmbed()
     .setTitle('**Added to queue**')
     .setDescription(`[${song.name}](${song.url})`)
@@ -63,35 +94,4 @@ client.on("ready", () => {
         console.error(e)
         message.channel.send("An error encountered: " + e);
     });
-})
-
-client.on('guildMemberAdd', member => {
-    member.send('**' + member.user.username + '** Welcome to the server!');
-});
-
-client.on("message", async message => {
-   
-
-    if (message.author.bot) return;
-    if (!message.guild) return;
-    if (!message.content.startsWith(prefix)) return;
-
-    // If message.member is uncached, cache it.
-    if (!message.member) message.member = await message.guild.fetchMember(message);
-
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const cmd = args.shift().toLowerCase();
-    
-    if (cmd.length === 0) return;
-    
-    // Get the command
-    let command = client.commands.get(cmd);
-    // If none is found, try to find it by alias
-    if (!command) command = client.commands.get(client.aliases.get(cmd));
-
-    // If a command is finally found, run the command
-    if (command) 
-        command.run(client, message, args);
-});
-
 client.login(process.env.token);
