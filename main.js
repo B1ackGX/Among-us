f86362f6572af53d3df19143e6d0bbb0378a0ed7
B1ackGX <xueqianlin.0821@gmail.com>
2021-09-25 15:26:18 -0700

    Update main.js

--- main.js
+++ main.js
@@ -54,11 +54,9 @@ client.on("message", async message => {
 });
 
     client.distube
-    .on("playSong", (message, queue, song) => {if (queue.songs[0] && queue.songs.length >= 1){
-        message.channel.send(`**Playing**:notes: \`${song.name}\` - Now! `)
-    } else {
-        return
-    }
+    .on("playSong", (message, queue, song) => {if (queue.songs[0].name == song.name){
+        return message.channel.send(`**Playing**:notes: \`${song.name}\` - Now! `)
+    } 
     })
     .on("addSong", (message, queue, song) => message.channel.send(new Discord.MessageEmbed()
     .setTitle('**Added to queue**')
