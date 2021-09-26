de7ca6a48d901e9852bf341d161823829550b70c
B1ackGX <xueqianlin.0821@gmail.com>
2021-09-26 00:41:55 -0700

    Update queue.js

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -17,14 +17,13 @@ module.exports = {
 
         if(queue.songs[0]) string += `__Now Playing:__\n [${queue.songs[0].name}](${queue.songs[0].url})\n \`${queue.songs[0].formattedDuration} Requested by: ${queue.songs[0].user.tag}\``
         if(queue.songs[1]) string += `\n__Up Next:__\n ${queue.songs.map((song, id) => `\`${id}.\` [${song.name}](${song.url})\n \`${song.formattedDuration} Requested by: ${song.user.tag}\``).slice(1, 10).join("\n")}`
-        
-        const pages = []
 
         const embed = new Discord.MessageEmbed()
         .setTitle(`**Queue for ${message.guild}**`)
         .setDescription(string)
         .setColor('RANDOM')
         .addField("\u200B", `**${queue.songs.length} songs in queue | ${queue.formattedDuration} total length**`)
+        
         const pages = [
             embed
         ]
