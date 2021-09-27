825fb71a00ae274e3bf46353580fe4d57aa6fe6a
B1ackGX <xueqianlin.0821@gmail.com>
2021-09-26 20:40:50 -0700

    Update queue.js

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -18,28 +18,15 @@ module.exports = {
         if(queue.songs[0]) string += `__Now Playing:__\n [${queue.songs[0].name}](${queue.songs[0].url})\n \`${queue.songs[0].formattedDuration} Requested by: ${queue.songs[0].user.tag}\``
         if(queue.songs[1]) string += `\n__Up Next:__\n ${queue.songs.map((song, id) => `\`${id}.\` [${song.name}](${song.url})\n \`${song.formattedDuration} Requested by: ${song.user.tag}\``).slice(1, 10).join("\n")}`
 
-        const embeds = generateEmbed(queue)
-
-        function generateEmbed(queue) {
-        const embeds = [];
-        let k = 10;
-        for(let i = 0; i< queue.songs.length; i+= 10) {
-        k += 10;
         const embed = new Discord.MessageEmbed()
         .setTitle(`**Queue for ${message.guild}**`)
         .setDescription(string)
         .setColor('RANDOM')
         .addField("\u200B", `**${queue.songs.length} songs in queue | ${queue.formattedDuration} total length**`)
-        embeds.push(embed);
-        }
-        return embeds;
-        }
+       
 
-        /*
-        
-        */
         const pages = [
-            embeds
+            embed
         ]
         
         const emoji = ["⏪", "⏩"]
