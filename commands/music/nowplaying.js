419539fb370390e5e7cc7f5e3991fa0a9a472f10
B1ackGX <xueqianlin.0821@gmail.com>
2021-10-02 23:08:25 -0700

    Update nowplaying.js

--- commands/music/nowplaying.js
+++ commands/music/nowplaying.js
@@ -24,8 +24,8 @@ module.exports = {
         .setTitle('**Now Playing** ♪')
         .setThumbnail(`${queue.songs[0].thumbnail}`)
         .setDescription(`[${queue.songs[0].name}](${queue.songs[0].url})`)
-        .addField(`${splitbar[0]}`, `\`${queue.formattedCurrentTime} / ${queue.songs[0].formattedDuration}\``)
-        .addField('\`Requested by:\` ' + queue.songs[0].user.tag)
+        .addFields({name: `${splitbar[0]}`, value: `\`${queue.formattedCurrentTime} / ${queue.songs[0].formattedDuration}\``})
+        .addField('\`Requested by:\` ', queue.songs[0].user.tag, true,)
         message.channel.send(embed);
     }
 }
