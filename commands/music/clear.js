9a4f7bd07b60971391c0b157469a00269bbd24bf
B1ackGX <xueqianlin.0821@gmail.com>
2021-10-03 23:42:17 -0700

    Update clear.js

--- commands/music/clear.js
+++ commands/music/clear.js
@@ -4,13 +4,13 @@ module.exports = {
     name: "clear",
     description: "clear the whole queue",
     run: async (client, message, args) => {
+        const queue = client.distube.getQueue(message)
         if(!message.member.voice.channel){
             return message.channel.send('❌ **Please join the voice channel!**');
         }
         if(client.distube.isPlaying(message) && message.member.voice.channel != message.guild.me.voice.channel){
             return message.channel.send('❌ **You are not in the same voice channel as I am!**')
         }
-        const queue = client.distube.getQueue(message)
         if (!queue) {
             return message.channel.send(`**There is nothing playing!**`)
         }
