a093eff7a2147d546647ab22d48905f1ec9a4745
B1ackGX <xueqianlin.0821@gmail.com>
2021-10-04 22:49:38 -0700

    Update queue.js

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -12,16 +12,6 @@ module.exports = {
         .setDescription(`__Now Playing:__\nNothing Playing!`)
         .setColor('RANDOM')
         );
-        
-        let string = "";
-        if(queue.songs[0]) string += `__Now Playing:__\n [${queue.songs[0].name}](${queue.songs[0].url})\n \`${queue.songs[0].formattedDuration} Requested by: ${queue.songs[0].user.tag}\``
-        if(queue.songs[1]) string += `\n__Up Next:__\n ${queue.songs.map((song, id) => `\`${id}.\` [${song.name}](${song.url})\n \`${song.formattedDuration} Requested by: ${song.user.tag}\``).slice(1, 10).join("\n")}`
-        const embed = new Discord.MessageEmbed()
-        .setTitle(`**Queue for ${message.guild}**`)
-        .setDescription(string)
-        .setColor('RANDOM')
-        .addField("\u200B", `**${queue.songs.length} songs in queue | ${queue.formattedDuration} total length**`)
-        .setFooter('Page 1/1')
 
         const pages = generateQueueEmbed(queue)
         function generateQueueEmbed(queue){
@@ -41,11 +31,7 @@ module.exports = {
             }
             return pages;
         }
-        if(pages.length <= 10){
-            message.channel.send(embed)
-        } else{
         const emoji = ["⏪", "⏩"]
         pagination(message, pages, emoji, 60000);
-        }
     }
 }
