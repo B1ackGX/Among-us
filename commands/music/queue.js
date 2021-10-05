cef7c04b46dbeea5a383e5b59fefbfe689abe5e3
B1ackGX <xueqianlin.0821@gmail.com>
2021-10-04 20:39:19 -0700

    Update queue.js

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -19,7 +19,7 @@ module.exports = {
             const pages = []
             let k = 10;
             for(let i = 0; i < queue.songs.length; i += 10){
-                const current = queue.slice(i, k)
+                const current = queue.songs.slice(i, k)
                 let j = i + 1;
                 k += 10;
                 const info = current.map(song => `\`${++j}.\` [${song.name}](${song.url})\n \`${song.formattedDuration} Requested by: ${song.user.tag}\``).join('\n');
