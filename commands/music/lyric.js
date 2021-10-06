50590fb1da2010ce339c47a0419e61e5b1827bf2
B1ackGX <xueqianlin.0821@gmail.com>
2021-10-05 23:57:56 -0700

    lyric

---
+++ commands/music/lyric.js
@@ -0,0 +1,63 @@
+const Discord = require('discord.js');
+const lyricFinder = require('lyrics-finder');
+
+module.exports = {
+    name: "queue",
+    aliases: ['q'],
+    description: "Check the current queue!",
+    run: async (client, message, args) => {
+        const queue = client.distube.getQueue(message)
+        if (args.length < 1) {
+        return message.channel.send(new Discord.MessageEmbed()
+        .setTitle(`❌ **Missing Songs**`)
+        .setDescription(`.lyric [Song Name]`)
+        .setColor('GREEN')
+        )}
+        
+        let artist = args.join(" ")
+        let songName = ''
+        let pages = []
+        let currentPage = 0
+
+        const messageFilter = m => m.author.id === message.author.id
+        const reactionFilter = (reaction, user) => reaction['⏪', '⏩'].includes(reaction.emoji.name) && !user.bot
+        
+        message.channel.send("Please enter the song name now")
+        await message.channel.awaitMessage(messageFilter, { max: 1, time: 15000 }).then(async collected => { 
+            songName = collected.first().content
+            await finder (artist, songName, message, pages)  
+        })
+
+        const lyricEmbed = await message.channel.send(pages[currentPage])
+        await lyricEmbed.react('⏪')
+        await lyricEmbed.react('⏩')
+
+        const collector = lyricEmbed.createReactionCollector(reactionFilter)
+
+        collector.on('collection', (reaction, user) => {
+            if(reaction.emoji.name === '⏩'){
+                if(currentPage < pages.length-1){
+                    currentPage+=1
+                    lyricEmbed.edit(pages[currentPage])
+                }
+            }else if(reaction.emoji.name === '⏪'){
+                if(currentPage !== 0){
+                    currentPage-=1
+                    lyricEmbed.edit(pages[currentPage])
+                }
+            }
+        })
+        async function finder(artist, songName, message, pages){
+            let fullLyric = await lyricFinder(artist, songName) || 'Not Found!';
+            
+            for (let i = 0; i < fullLyric.length; i+= 2048) {
+                const lyric = fullLyric.substring(i, Math.min(fullLyric.length, i + 2048))
+                const message = new Discord.MessageEmbed()
+                    .setDescription(lyric)
+                    .setThumbnail(queue.songs[0].thumbnail)
+                    .setColor('GREEN')
+                pages.push(message)
+            }
+        }
+    }
+}
