dda141b227d71d55f2ac2bb963b1acc28efdfee4
B1ackGX <xueqianlin.0821@gmail.com>
2021-12-11 06:38:27 -0800

    Update slap.js

--- commands/fun/slap.js
+++ commands/fun/slap.js
@@ -19,7 +19,10 @@ module.exports = {
             'https://c.tenor.com/wOCOTBGZJyEAAAAC/chikku-neesan-girl-hit-wall.gif',
             'https://c.tenor.com/Sp7yE5UzqFMAAAAS/spank-slap.gif',
             'https://c.tenor.com/ra17G61QRQQAAAAC/tapa-slap.gif',
-            'https://c.tenor.com/CAesvxP0KyEAAAAd/shinobu-kocho-giyuu-tomioka.gif'
+            'https://c.tenor.com/CAesvxP0KyEAAAAd/shinobu-kocho-giyuu-tomioka.gif',
+            'https://c.tenor.com/FaXcxpmU3PMAAAAC/anime-slap.gif',
+            'https://c.tenor.com/Irk80uToJA0AAAAC/slap-anime.gif',
+            'https://c.tenor.com/2HjyotNxqiAAAAAC/cass-will.gif'
         ]
 
         let embed = new Discord.MessageEmbed()
