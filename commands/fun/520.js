fab184629b7d5123b5006f9cf213083783afc676
B1ackGX <xueqianlin.0821@gmail.com>
2021-12-11 05:26:02 -0800

    仙女专属更新 <3

---
+++ commands/fun/520.js
@@ -0,0 +1,22 @@
+const discord = require('discord.js');
+
+module.exports = {
+    name: "520",
+    category: "fun",
+    description: "520黄诗萍",
+    run: async (client, message, args) => {
+
+        let embed = new discord.MessageEmbed()
+        .setTitle('❤️黄诗萍我爱你❤️')
+        .setImage('https://c.tenor.com/Gpg2Fv5w_V8AAAAM/heart-anime.gif')
+        .setcolor('PURPLE')
+        .setTimestamp()
+        message.channel.send(embed)
+        message.channel.send('黄诗萍我余生都会爱你\n 这一辈子我都想和你在一起\n 不离不弃\n ❤️爱你❤️')
+        message.channel.send(new discord.MessageEmbed()
+        .setImage('https://c.tenor.com/7T1cuiOtJvQAAAAC/anime-kiss.gif')
+        .setcolor('PURPLE')
+        .setTimestamp()
+        )
+    }
+}
