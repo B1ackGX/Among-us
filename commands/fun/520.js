8961e1b424f5029c081dbd5d4aac68f04580842d
B1ackGX <xueqianlin.0821@gmail.com>
2021-12-11 05:43:36 -0800

    Update 520.js

--- commands/fun/520.js
+++ commands/fun/520.js
@@ -11,12 +11,18 @@ module.exports = {
         .setImage('https://c.tenor.com/Gpg2Fv5w_V8AAAAM/heart-anime.gif')
         .setColor('PURPLE')
         .setTimestamp()
-        message.channel.send(embed)
-        message.channel.send('黄诗萍我余生都会爱你\n 这一辈子我都想和你在一起\n 不离不弃\n ❤️爱你❤️')
-        message.channel.send(new discord.MessageEmbed()
-        .setImage('https://c.tenor.com/7T1cuiOtJvQAAAAC/anime-kiss.gif')
-        .setColor('PURPLE')
-        .setTimestamp()
+        message.channel.send(embed).then(msg => {
+            setTimeout(function(){ 
+                message.channel.send('黄诗萍我余生都会爱你\n 这一辈子我都想和你在一起\n 不离不弃\n ❤️爱你❤️').then(msg =>{
+                    setTimeout(function(){ 
+                        message.channel.send(new discord.MessageEmbed()
+                        .setImage('https://c.tenor.com/7T1cuiOtJvQAAAAC/anime-kiss.gif')
+                        .setColor('PURPLE')
+                        .setTimestamp()
         )
+                    }, 3000);
+                })
+            }, 3000);
+        })
     }
 }
