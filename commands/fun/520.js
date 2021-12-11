9a2bf859db5850dd3ca74ed0975d8beae1bd0602
B1ackGX <xueqianlin.0821@gmail.com>
2021-12-11 05:35:25 -0800

    Update 520.js

--- commands/fun/520.js
+++ commands/fun/520.js
@@ -6,10 +6,16 @@ module.exports = {
     description: "520黄诗萍",
     run: async (client, message, args) => {
 
-        message.channel.send(new discord.MessageEmbed()
+        let embed = new discord.MessageEmbed()
         .setTitle('❤️黄诗萍我爱你❤️')
         .setImage('https://c.tenor.com/Gpg2Fv5w_V8AAAAM/heart-anime.gif')
-        .setcolor('PURPLE')
+        .setColor('PURPLE')
+        .setTimestamp()
+        message.channel.send(embed)
+        message.channel.send('黄诗萍我余生都会爱你\n 这一辈子我都想和你在一起\n 不离不弃\n ❤️爱你❤️')
+        message.channel.send(new discord.MessageEmbed()
+        .setImage('https://c.tenor.com/7T1cuiOtJvQAAAAC/anime-kiss.gif')
+        .setColor('PURPLE')
         .setTimestamp()
         )
     }
